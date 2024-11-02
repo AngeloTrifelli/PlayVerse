2298647c1708075dc913b3aefdfbea5f43692bc5
AngeloTrifelli <trifelli.1920939@studenti.uniroma1.it>
2024-11-02 13:54:29 +0100

    Fixed admin password

--- Database/insertData.sql
+++ Database/insertData.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE insert_data()
 
 BEGIN 
     IF NOT EXISTS (SELECT 1 FROM User LIMIT 1) THEN
-        INSERT INTO User VALUES (1, 'admin', 'admin', 'ADMIN', 'adminName', 'adminSurname', '2000-08-27', 0, 10000, '2000-08-27', 0, NULL, 0, NULL);
+        INSERT INTO User VALUES (1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'ADMIN', 'adminName', 'adminSurname', '2000-08-27', 0, 10000, '2000-08-27', 0, NULL, 0, NULL);
     END IF;
 END $$
 
